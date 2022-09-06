3b200b15631d646e65d57b130b78f7b516ae9318
eva <ebassi22@student.aau.dk>
2022-09-06 08:48:26 -0700

    jeg har tilføjet en forlykke

--- main.c
+++ main.c
@@ -3,5 +3,9 @@
 int main() {
     printf("Hello, World!\n");
     printf("Hello, World!\n");
+    int a = 50, i = 0;
+    for(i = 0; i <= a; i++){
+        printf("Antal gange %d\n", i);
+    }
     return 0;
 }
